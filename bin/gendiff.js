0c00a55e5ff22efb10ac0fb489644f88a336feeb
mikhail manzik <mikhailmanzik@yandex.ru>
2021-08-19 14:49:24 +0200

    Подключает линтер.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
-import fs from 'fs';
-import path from 'path';
 import _ from 'lodash';
+import genDiff from '../src/index.js';
+import readFile from '../src/utils.js';
 
 const { Command } = commander;
 const program = new Command();
-
-const readFile = (filePath) => {
-  const currentDirecory = process.cwd();
-  const fullPath = path.resolve(currentDirecory, filePath);
-  return fs.readFileSync(fullPath, { encoding: 'utf8' });
-};
 
 program
   .helpOption('-h, --help', 'output usage information')
@@ -22,29 +16,15 @@ program
   .action((filepath1, filepath2) => {
     const file1 = readFile(filepath1);
     const file2 = readFile(filepath2);
-    const parsFile1 = JSON.parse(file1);
-    const parsFile2 = JSON.parse(file2);
+    const parsedFile1 = JSON.parse(file1);
+    const parsedFile2 = JSON.parse(file2);
 
-    const keys1 = Object.keys(parsFile1);
-    const keys2 = Object.keys(parsFile2);
+    const keys1 = Object.keys(parsedFile1);
+    const keys2 = Object.keys(parsedFile2);
 
     const sumKeys = _.union(keys1, keys2).sort();
 
-    const genDiff = (key) => {
-      let str = '';
-      if (parsFile1[key] === parsFile2[key]) {
-        str = `  ${key}: ${parsFile1[key]}\n`;
-      } if (parsFile1[key] !== parsFile2[key]) {
-        str = `- ${key}: ${parsFile1[key]}\n + ${key}: ${parsFile2[key]}\n`;
-      } if (!_.has(parsFile1, key)) {
-        str = `+ ${key}: ${parsFile2[key]}\n`;
-      } if (!_.has(parsFile2, key)) {
-        str = `- ${key}: ${parsFile1[key]}\n`;
-      }
-      return str;
-    };
-
-    const result = sumKeys.map((key) => genDiff(key)).join(' ');
+    const result = sumKeys.map((key) => genDiff(key, parsedFile1, parsedFile2)).join(' ');
     console.log(`{\n ${result}}`);
   });
 
